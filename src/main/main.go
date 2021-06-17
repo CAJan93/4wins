cc12e3badc6a2c8d0bfb23ad080ce57584ca7605
Jan Mensch <jan.mensch@sap.com>
2021-06-17 23:14:15 +0200

    handle user input

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -57,7 +59,7 @@ func (g *game) printBoard() {
 
 // init initializes the board and sets all fields to " "
 func (g *game) init() {
-	g.PlayersTurn = 0
+	g.PlayersTurn = 1
 	g.Board = make([][]string, 6)
 	for i, _ := range g.Board {
 		g.Board[i] = make([]string, 5)
@@ -69,22 +71,47 @@ func (g *game) init() {
 
 // selectComputerMove selects a random number
 func (g *game) selectComputerMove() int {
+	fmt.Println("Computer move")
+	time.Sleep(1 * time.Second)
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(6)
+	return rand.Intn(5)
 }
 
 // selectHumanMove gets a move from the user
-func (g *game) selectHumanMove() int {
-	// TODO
-	return 0
+func (g *game) selectHumanMove(reader *bufio.Reader) (int, error) {
+	fmt.Println("Please select a column")
+	var userInput int
+	_, err := fmt.Scanf("%d", &userInput)
+	if err != nil {
+		return -1, err
+	}
+	if userInput < 0 || userInput > 4 {
+		return -1, fmt.Errorf("please select a number between 0 and 4 inclusive")
+	}
+	return userInput, nil
 }
 
 // selectMove wraps doComputerMove and doHumanMove
-func (g *game) selectMove() int {
+func (g *game) selectMove(reader *bufio.Reader) int {
 	if g.PlayersTurn == 0 {
-		return g.selectHumanMove()
+		num, err := g.selectHumanMove(reader)
+		if err != nil {
+			fmt.Println(err) // TODO remove line
+			fmt.Println("Invalid input, please try again")
+			g.selectMove(reader)
+		}
+		return num
 	}
 	return g.selectComputerMove()
+}
+
+// doMove does executes a selected move on the board
+func (g *game) doMove(column int) {
+	if g.PlayersTurn == 0 {
+		g.Board[0][column] = "X"
+		return
+	}
+	g.Board[0][column] = "O"
 }
 
 // TODO
@@ -98,11 +125,16 @@ func main() {
 	g.init()
 	g.printHelp()
 
+	ioReader := bufio.NewReader(os.Stdin)
+
 	// game loop
 	gameFinished := false
 	for !gameFinished {
 		// players do action
-		g.selectMove()
+		columnSelected := g.selectMove(ioReader)
+		g.doMove(columnSelected)
+		g.printBoard()
 		gameFinished, _ = g.won()
+		g.alternatePlayersTurn()
 	}
 }
