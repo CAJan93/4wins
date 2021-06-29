55c2d95e10ff4092b354e029dcca1f882110c39e
Jan Mensch <jan.mensch@sap.com>
2021-06-29 23:41:07 +0200

    add horizontal won test

--- src/main/main_test.go
+++ src/main/main_test.go
@@ -1,6 +1,9 @@
-package game
+package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestUsableBoard(t *testing.T) {
 	g := getNewGame()
@@ -27,4 +30,54 @@ func TestCheckNextXHorizontal(t *testing.T) {
 	}
 
 	g.init()
+}
+
+func TestWon(t *testing.T) {
+	type testHelper struct {
+		xPos []int
+		yPos []int
+	}
+
+	var g game
+	g.init()
+
+	scenarios := [...]testHelper{
+		// horizontal
+		{[]int{0, 0, 0, 0}, []int{0, 1, 2, 3}},
+		{[]int{0, 0, 0, 0}, []int{1, 2, 3, 4}},
+		{[]int{0, 0, 0, 0}, []int{2, 3, 4, 5}},
+		{[]int{1, 1, 1, 1}, []int{0, 1, 2, 3}},
+		{[]int{1, 1, 1, 1}, []int{1, 2, 3, 4}},
+		{[]int{1, 1, 1, 1}, []int{2, 3, 4, 5}},
+		{[]int{2, 2, 2, 2}, []int{0, 1, 2, 3}},
+		{[]int{2, 2, 2, 2}, []int{1, 2, 3, 4}},
+		{[]int{2, 2, 2, 2}, []int{2, 3, 4, 5}},
+		{[]int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}, []int{0, 1, 2, 3}},
+		{[]int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}, []int{1, 2, 3, 4}},
+		{[]int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}, []int{2, 3, 4, 5}},
+
+		// vertical
+		// diagonal missing
+	}
+
+	for _, scenario := range scenarios {
+		for i, xpos := range scenario.xPos {
+			g.Board[xpos][scenario.yPos[i]] = playerIntToStrig(0)
+		}
+		won, winning_player := g.won()
+		if !won {
+			g.printBoard()
+			msg := "Expected a player to have one, but was not the case. See above for game filed"
+			fmt.Println(msg)
+			t.Errorf(msg)
+		}
+		if won && int(winning_player) != 0 {
+			g.printBoard()
+			msg := "Expected a player 0 to have one, but was not the case. See above for game filed"
+			fmt.Println(msg)
+			t.Errorf(msg)
+		}
+
+		g.init()
+	}
 }
