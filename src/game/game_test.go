531b5bb8726035ace4c25bb3b623d1edc0388a04
Jan Mensch <jan.mensch@sap.com>
2021-07-04 23:28:36 +0200

    benchmark won

--- src/game/game_test.go
+++ src/game/game_test.go
@@ -6,23 +6,6 @@ import (
 	"testing"
 )
 
-// Try to catch the error that shows up when first slecting an invalid column
-// Please select a column
-// 9
-// Invalid input, please try again
-// Please select a column
-// 3
-// panic: runtime error: index out of range [-1]
-//
-// goroutine 1 [running]:
-// fourwins/main/src/game.(*Game).fall(0xc0000a1e98, 0xffffffffffffffff, 0x10b8d16, 0xc0000a1e98, 0xc0000a1f20)
-//         /Users/D072532/go/src/4Gewinnt/src/game/game.go:242 +0x16e
-// fourwins/main/src/game.(*Game).DoMove(0xc0000a1e98, 0xffffffffffffffff, 0xffffffffffffffff, 0x0)
-//         /Users/D072532/go/src/4Gewinnt/src/game/game.go:256 +0x3f
-// main.main()
-//         /Users/D072532/go/src/4Gewinnt/src/main/main.go:21 +0x24f
-// make: *** [run] Error 2
-
 func TestUsableBoard(t *testing.T) {
 	g := GetNewGame()
 	if g.Width < 4 {
@@ -62,6 +45,15 @@ func Test_selectComputerMoveInBound(t *testing.T) {
 	}
 }
 
+func Benchmark_minmax(b *testing.B) {
+	var g Game
+	g.init()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		minmax(g, 5, misc.Player(0))
+	}
+}
+
 // Tests obvious decisions, where 3 objects are in a row already
 func Test_selectComputerMoveDecision(t *testing.T) {
 	type testHelper struct {
@@ -71,16 +63,16 @@ func Test_selectComputerMoveDecision(t *testing.T) {
 	}
 	var g Game
 	g.init()
-	bottom := g.Height - 1
+	bottom := gHeight() - 1
 
 	scenarios := [...]testHelper{
 		// horizontal
 		{[]int{0, 1, 2}, []int{bottom, bottom, bottom}, 3},
-		{[]int{g.Width - 1, g.Width - 2, g.Width - 3}, []int{bottom, bottom, bottom}, g.Width - 4},
+		{[]int{gWidth() - 1, g.Width - 2, g.Width - 3}, []int{bottom, bottom, bottom}, g.Width - 4},
 
 		// vertical
 		{[]int{0, 0, 0}, []int{bottom, bottom - 1, bottom - 2}, 0},
-		{[]int{g.Width - 1, g.Width - 1, g.Width - 1}, []int{bottom, bottom - 1, bottom - 2}, g.Width - 1},
+		{[]int{gWidth() - 1, gWidth() - 1, gWidth() - 1}, []int{bottom, bottom - 1, bottom - 2}, gWidth() - 1},
 	}
 
 	// do for both players
@@ -101,49 +93,62 @@ func Test_selectComputerMoveDecision(t *testing.T) {
 	}
 }
 
+type testHelper struct {
+	xPos []int
+	yPos []int
+}
+
+func gHeight() int {
+	var g Game
+	g.init()
+	return g.Height
+}
+
+func gWidth() int {
+	var g Game
+	g.init()
+	return g.Width
+}
+
+var scenariosTestWon = [...]testHelper{
+	// horizontal
+	{[]int{0, 1, 2, 3}, []int{0, 0, 0, 0}},
+	{[]int{1, 2, 3, 4}, []int{0, 0, 0, 0}},
+	{[]int{2, 3, 4, 5}, []int{0, 0, 0, 0}},
+	{[]int{0, 1, 2, 3}, []int{1, 1, 1, 1}},
+	{[]int{1, 2, 3, 4}, []int{1, 1, 1, 1}},
+	{[]int{2, 3, 4, 5}, []int{1, 1, 1, 1}},
+	{[]int{0, 1, 2, 3}, []int{2, 2, 2, 2}},
+	{[]int{1, 2, 3, 4}, []int{2, 2, 2, 2}},
+	{[]int{2, 3, 4, 5}, []int{2, 2, 2, 2}},
+	{[]int{0, 1, 2, 3}, []int{gHeight() - 1, gHeight() - 1, gHeight() - 1, gHeight() - 1}},
+	{[]int{1, 2, 3, 4}, []int{gHeight() - 1, gHeight() - 1, gHeight() - 1, gHeight() - 1}},
+	{[]int{2, 3, 4, 5}, []int{gHeight() - 1, gHeight() - 1, gHeight() - 1, gHeight() - 1}},
+	{[]int{0, 1, 2, 3, 4, 5}, []int{gHeight() - 1, gHeight() - 1, gHeight() - 1, gHeight() - 1, gHeight() - 1, gHeight() - 1}},
+
+	// vertical
+	{[]int{0, 0, 0, 0}, []int{0, 1, 2, 3}},
+	{[]int{0, 0, 0, 0}, []int{1, 2, 3, 4}},
+	{[]int{0, 0, 0, 0}, []int{2, 3, 4, 5}},
+	{[]int{0, 0, 0, 0}, []int{gHeight() - 1, gHeight() - 2, gHeight() - 3, gHeight() - 4}},
+	{[]int{gWidth() - 1, gWidth() - 1, gWidth() - 1, gWidth() - 1}, []int{0, 1, 2, 3}},
+	{[]int{gWidth() - 1, gWidth() - 1, gWidth() - 1, gWidth() - 1}, []int{1, 2, 3, 4}},
+	{[]int{gWidth() - 1, gWidth() - 1, gWidth() - 1, gWidth() - 1}, []int{2, 3, 4, 5}},
+	{[]int{gWidth() - 1, gWidth() - 1, gWidth() - 1, gWidth() - 1}, []int{gHeight() - 1, gHeight() - 2, gHeight() - 3, gHeight() - 4}},
+
+	// diagonal missing
+	{[]int{1, 2, 3, 4}, []int{1, 2, 3, 4}},
+	{[]int{2, 3, 4, 5}, []int{2, 3, 4, 5}},
+	{[]int{5, 4, 3, 2}, []int{2, 3, 4, 5}},
+	{[]int{4, 3, 2, 1}, []int{1, 2, 3, 4}},
+}
+
 func TestWon(t *testing.T) {
-	type testHelper struct {
-		xPos []int
-		yPos []int
-	}
 
 	var g Game
 	g.init()
 
-	scenarios := [...]testHelper{
-		// horizontal
-		{[]int{0, 1, 2, 3}, []int{0, 0, 0, 0}},
-		{[]int{1, 2, 3, 4}, []int{0, 0, 0, 0}},
-		{[]int{2, 3, 4, 5}, []int{0, 0, 0, 0}},
-		{[]int{0, 1, 2, 3}, []int{1, 1, 1, 1}},
-		{[]int{1, 2, 3, 4}, []int{1, 1, 1, 1}},
-		{[]int{2, 3, 4, 5}, []int{1, 1, 1, 1}},
-		{[]int{0, 1, 2, 3}, []int{2, 2, 2, 2}},
-		{[]int{1, 2, 3, 4}, []int{2, 2, 2, 2}},
-		{[]int{2, 3, 4, 5}, []int{2, 2, 2, 2}},
-		{[]int{0, 1, 2, 3}, []int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}},
-		{[]int{1, 2, 3, 4}, []int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}},
-		{[]int{2, 3, 4, 5}, []int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}},
-		{[]int{0, 1, 2, 3, 4, 5}, []int{g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1, g.Height - 1}},
-
-		// vertical
-		{[]int{0, 0, 0, 0}, []int{0, 1, 2, 3}},
-		{[]int{0, 0, 0, 0}, []int{1, 2, 3, 4}},
-		{[]int{0, 0, 0, 0}, []int{2, 3, 4, 5}},
-		{[]int{0, 0, 0, 0}, []int{g.Height - 1, g.Height - 2, g.Height - 3, g.Height - 4}},
-		{[]int{g.Width - 1, g.Width - 1, g.Width - 1, g.Width - 1}, []int{0, 1, 2, 3}},
-		{[]int{g.Width - 1, g.Width - 1, g.Width - 1, g.Width - 1}, []int{1, 2, 3, 4}},
-		{[]int{g.Width - 1, g.Width - 1, g.Width - 1, g.Width - 1}, []int{2, 3, 4, 5}},
-		{[]int{g.Width - 1, g.Width - 1, g.Width - 1, g.Width - 1}, []int{g.Height - 1, g.Height - 2, g.Height - 3, g.Height - 4}},
-
-		// diagonal missing
-		{[]int{1, 2, 3, 4}, []int{1, 2, 3, 4}},
-		{[]int{2, 3, 4, 5}, []int{2, 3, 4, 5}},
-		{[]int{5, 4, 3, 2}, []int{2, 3, 4, 5}},
-		{[]int{4, 3, 2, 1}, []int{1, 2, 3, 4}},
-	}
-
-	for _, scenario := range scenarios {
+	for _, scenario := range scenariosTestWon {
 		for i, xpos := range scenario.xPos {
 			g.Board[scenario.yPos[i]][xpos] = misc.PlayerIntToStrig(0)
 		}
@@ -162,5 +167,14 @@ func TestWon(t *testing.T) {
 		}
 
 		g.init()
+	}
+}
+
+func BenchmarkWon(b *testing.B) {
+	var g Game
+	g.init()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		g.Won()
 	}
 }
